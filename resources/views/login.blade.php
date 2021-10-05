d89d1233f2a53846fd10164605d3d27fd4016149
htethtetoo20 <migely16@gmail.com>
2021-10-05 19:14:02 +0630

    header,footer and login page is done

---
+++ resources/views/login.blade.php
@@ -0,0 +1,21 @@
+@extends("master")
+@section("content")
+    <div class="container size">
+    <form>
+        <div class="row mt-3 mb-3 offset-4">
+            <label for="inputEmail3" class="col-sm-2 col-form-label">Email</label>
+            <div class="col-sm-5">
+                <input type="email" class="form-control" id="inputEmail3">
+            </div>
+        </div>
+        <div class="row mb-3 offset-4">
+            <label for="inputPassword3" class="col-sm-2 col-form-label">Password</label>
+            <div class="col-sm-5">
+                <input type="password" class="form-control" id="inputPassword3">
+            </div>
+        </div>
+
+        <button type="submit" class="btn btn-primary offset-4">Sign in</button>
+    </form>
+    </div>
+@endsection

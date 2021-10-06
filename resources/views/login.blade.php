d457580ee4535c5640042c40bd5f64733c12aded
root <migely16@gmail.com>
2021-10-06 15:00:36 +0630

    login done

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,17 +1,18 @@
 @extends("master")
 @section("content")
     <div class="container size">
-    <form>
+    <form action="login" method="post">
+        @csrf
         <div class="row mt-3 mb-3 offset-4">
             <label for="inputEmail3" class="col-sm-2 col-form-label">Email</label>
             <div class="col-sm-5">
-                <input type="email" class="form-control" id="inputEmail3">
+                <input type="email" name="email" class="form-control" id="inputEmail3">
             </div>
         </div>
         <div class="row mb-3 offset-4">
-            <label for="inputPassword3" class="col-sm-2 col-form-label">Password</label>
+            <label for="inputPassword3"  class="col-sm-2 col-form-label">Password</label>
             <div class="col-sm-5">
-                <input type="password" class="form-control" id="inputPassword3">
+                <input type="password" name="password" class="form-control" id="inputPassword3">
             </div>
         </div>
 
